8ce23bdca1377980f66c17fd750fa546ec510d90
Santholin <santholin27@gmail.com>
2025-04-25 12:08:47 +0530

    add: index file

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SANTHOLINS FURTADO</title>
+</head>
+<body>
+// <title>SANTHOLINS FURTADO</title>
+    <h1>Welcome to Autos Database</h1>
+
+    <p><a href="login.php">Please Log In</a></p>
+
+    <p>
+        Attempt to go to <a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
+    </p>
+
+    <p>
+        <a href="http://www.wa4e.com/assn/autosdb/">Specification for this Application</a>
+    </p>
+
+    <p>
+    
+        SANTHOLIN SFURTADO
+    </p>
+</body>
+</html>
